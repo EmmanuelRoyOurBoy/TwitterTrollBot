f55a4159875543256d02e3d8768b75e05726bd89
Emmanuel Roy <54725843+EmmanuelRoyOurBoy@users.noreply.github.com>
2020-01-05 17:22:17 -0600

    Update Example.py

--- Example troll script/Example.py
+++ Example troll script/Example.py
@@ -1,1 +1,12 @@
-
+import random
+Words to troll=input("What phrase do you want to trollify?)
+def Troll(text)
+  new = []
+  for c in text:
+    r = random.randint(0,1)
+    if r:
+        new.append(c.upper())
+    else:
+      new.append(c.lower())
+  return
+print(Troll(Words to troll)
